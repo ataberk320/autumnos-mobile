1a6af4a4f7df8ebab2e1f7c271586ee5f38a6bd6
ataberk320 <atatv183@gmail.com>
2026-03-22 17:47:51 +0300

    Refactor acore.h for consistency and clarity

--- rootfs_overlay/usr/bin/acore.h
+++ rootfs_overlay/usr/bin/acore.h
@@ -6,18 +6,18 @@
 #include <stdio.h>
 #include <string.h>
 
-// Sistem Sabitleri
+
 #define CRITICAL_TEMP_THRESHOLD 60.0f
 #define LOW_BATTERY_THRESHOLD 15
 
-// Global Değişkenler
+
 extern float cpu_temperature;
-extern char current_operator[64]; // Boyutu 64 yapalım, garanti olsun
+extern char current_operator[64];
 extern bool is_connected;
 extern bool sim_present;
 extern int uart_fd;
 extern int sim800_signal_level;
-// Fonksiyonlar
+
 void uart_init(void);
 void read_sim800_data(void);
 int read_battery_capacity(void);
