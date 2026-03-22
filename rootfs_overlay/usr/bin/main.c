aa6f198da37755c367d920d241b2c18e4f9066ca
ataberk320 <atatv183@gmail.com>
2026-03-22 17:49:35 +0300

    Refactor main.c for improved readability and structure

--- rootfs_overlay/usr/bin/main.c
+++ rootfs_overlay/usr/bin/main.c
@@ -19,7 +19,7 @@ void update_clock(lv_timer_t * timer) {
 
 	strftime(buffer, sizeof(buffer), "%H:%M", timeinfo);
 
-    // EEZ Studio'dan gelen objenin adının doğru olduğundan emin ol
+    /
     	if(objects.clocklabel != NULL) {
         lv_label_set_text(objects.clocklabel, buffer);
 	}
@@ -52,47 +52,45 @@ void system_monitor_task(lv_timer_t * timer) {
 
 int main(void) {
 
-    // 1. LVGL Çekirdeğini Başlat
+ 
     lv_init();
 
-    // 2. Linux Framebuffer Sürücüsünü Başlat (/dev/fb0)
+
     fbdev_init();
 
-    // 3. Çift Tampon (Double Buffering) - 1GB RAM Avantajı
-    // Her tamponu ekranın 1/4'ü kadar (200 satır) tutuyoruz. 
-    // Bu, MQ Pro'nun DMA (Direct Memory Access) performansını artırır.
+
     static lv_disp_draw_buf_t draw_buf;
     static lv_color_t buf1[480 * 200]; 
     static lv_color_t buf2[480 * 200]; 
     lv_disp_draw_buf_init(&draw_buf, buf1, buf2, 480 * 200);
 
-    // 4. Ekran Sürücüsü Yapılandırması
+
     static lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
-    disp_drv.flush_cb = fbdev_flush; // LVGL'den Framebuffer'a veri taşıyan köprü
+    disp_drv.flush_cb = fbdev_flush;
     disp_drv.draw_buf = &draw_buf;
-    disp_drv.hor_res = 480; // Dikey Genişlik
-    disp_drv.ver_res = 800; // Dikey Yükseklik
+    disp_drv.hor_res = 480;
+    disp_drv.ver_res = 800; 
     lv_disp_drv_register(&disp_drv);
     evdev_init();
     static lv_indev_drv_t indev_drv;
     lv_indev_drv_init(&indev_drv);
     indev_drv.type = LV_INDEV_TYPE_POINTER;
-    indev_drv.read_cb = evdev_read; // /dev/input/eventX üzerinden okuma yapar
+    indev_drv.read_cb = evdev_read; 
     lv_indev_drv_register(&indev_drv);
-    // 5. EEZ Studio Tasarımını Yükle
+
     ui_init();
     lv_timer_create(system_monitor_task, 1000, NULL);
     lv_obj_clear_flag(lv_scr_act(), LV_OBJ_FLAG_SCROLLABLE);
     
-    // Yandaki o gri sürükleme barlarını tamamen yok et
+
     lv_obj_set_scrollbar_mode(lv_scr_act(), LV_SCROLLBAR_MODE_OFF);
     lv_timer_create(update_clock, 1000, NULL);
-    // 6. Ana Döngü (Zamanlama Kritik!)
+
     while(1) {
-        lv_timer_handler(); // Grafik görevlerini yönetir
-        usleep(5000);       // 5ms uyku (İşlemciyi serin tutar)
-        lv_tick_inc(5);     // LVGL'ye 5ms geçtiğini bildirir
+        lv_timer_handler();
+        usleep(5000);
+        lv_tick_inc(5); 
 	
     }
 
