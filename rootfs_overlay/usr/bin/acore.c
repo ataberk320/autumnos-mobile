e2a5ad4eb3a0fe4471123a0ed7b4343e9fc20771
Autixberk <atatv183@gmail.com>
2026-04-07 20:40:18 +0300

    Update AutumnCore for reading request line.

--- rootfs_overlay/usr/bin/acore.c
+++ rootfs_overlay/usr/bin/acore.c
@@ -1,54 +1,76 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "atmhal.h"
 
 void mkdir_data(void) {
-        mkdir("/tmp/autumnsys", 0777);
-        mkdir("/tmp/autumnsys/battery", 0777);
-        mkdir("/tmp/autumnsys/uptime", 0777);
-        mkdir("/tmp/autumnsys/power", 0777);
+	mkdir("/tmp/autumnsys", 0777);
+    	mkdir("/tmp/autumnsys/battery", 0777);
+    	mkdir("/tmp/autumnsys/uptime", 0777);
+    	mkdir("/tmp/autumnsys/power", 0777);
 }
 
 void check_power_status(void) {
-	FILE *fp_pwr = fopen("/tmp/itstimetopoweroff", "r");
+	char line[64];
+	int req_val = -1;
+	FILE *fp_pwr = fopen("/tmp/autumnsys/power/itstimetopoweroff", "r");
 	if (fp_pwr != NULL) {
-		fclose(fp_pwr);
-		remove("/tmp/itstimetopoweroff");
+		while (fgets(line, sizeof(line), fp_pwr)) {
+			if (sscanf(line, "request=%d", &req_val) == 1) {
+				break;
+			}
+
+	}
+
+	fclose(fp_pwr);
+	remove("/tmp/autumnsys/power/itstimetopoweroff");
+	
+	if (req_val == 1) {
 		atmsys_pwroff();
-
 	}
+	req_val = -1;
+	FILE *fp_reb = fopen("/tmp/autumnsys/power/itstimetoreboot", "r");
+    	if (fp_reb != NULL) {
+        	while (fgets(line, sizeof(line), fp_reb)) {
+           		 if (sscanf(line, "request=%d", &req_val) == 1) {
+               		 break;
+            	}
+        }
 
-	FILE *fp_reb = fopen("/tmp/itstimetoreboot", "r");
-	if (fp_reb != NULL) {
-		fclose(fp_reb);
-		remove("/tmp/itstimetoreboot");
-		atmsys_reboot();
-	}
+        fclose(fp_reb);
+        remove("/tmp/autumnsys/power/itstimetoreboot");
+
+        if (req_val == 1) {
+            atmsys_reboot();
+        }
 }
 
 void update_system_status(void) {
 	int bat = atmsys_battery_perc();
-	FILE *fp_bat = fopen("/tmp/autumnbat0", "w");
+	FILE *fp_bat = fopen("/tmp/autumnsys/battery/autumnbat0", "w");
 	if (fp_bat != NULL)  {
 		fprintf(fp_bat, "%d", bat);
 		fclose(fp_bat);
 	}
 
 	long up = atmsys_uptime();
-    FILE *fp_up = fopen("/tmp/autumnuptime0", "w");
-    if (fp_up != NULL) {
-    if (up == -1) {
-        fprintf(fp_up, "HAL error");
-    }
-    else {
-        fprintf(fp_up, "%ld", up);
-    }
-    fclose(fp_up);
+	FILE *fp_up = fopen("/tmp/autumnsys/uptime/autumnuptime0", "w");
+	if (fp_up != NULL) {
+		if (up == -1) {
+			fprintf(fp_up, "HAL error");
+		}
+		else {
+			fprintf(fp_up, "%ld", up);
+		}
+		fclose(fp_up);
 }
 
 int main(void) {
+	atmsys_safe_volume(65);
 	atmsys_modemhdinit();
 	while (1) {
 		update_system_status();
