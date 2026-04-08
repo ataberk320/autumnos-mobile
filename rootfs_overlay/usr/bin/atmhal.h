e3d324cb5f43616e10a292eb1ac499907a6c7ac8
Autixberk <atatv183@gmail.com>
2026-04-08 21:36:04 +0300

    Add files via upload

--- rootfs_overlay/usr/bin/atmhal.h
+++ rootfs_overlay/usr/bin/atmhal.h
@@ -2,6 +2,15 @@
 #define ATMHAL_H
 #include <stdint.h>
 #include <stdbool.h>
+#include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void atmsys_set_brightness(uint8_t level);
 
 void atmsys_volume_up(void) ;
@@ -13,6 +22,10 @@ void atmsys_safe_volume(uint8_t volume);
 void playaudio(const char *path);
 
 int atmsys_camera_init(void);
+
+void atmsys_convert_videofrm(AVFrame *pFrame, AVCodecContext *pCodecCtx, unsigned char *out_buffer, int target_width, int target_height);
+
+void atmsys_play_video(const char *source, unsigned char *final_out_buffer, int start_second);
 
 //Power options
 void atmsys_reboot(void);
@@ -40,5 +53,7 @@ long atmsys_get_free_RAM(void);
 long atmsys_get_used_RAM(void);
 
 long atmsys_get_free_disk_space(const char *path);
-
+#ifdef __cplusplus
+}
+#endif
 #endif
