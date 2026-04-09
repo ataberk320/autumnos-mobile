db34d48c48d8f1a93e6a4ba677d79ad99559948c
Autixberk <atatv183@gmail.com>
2026-04-10 01:13:18 +0300

    Update g2d.mk

--- buildroot/g2d-lib/g2d.mk
+++ buildroot/g2d-lib/g2d.mk
@@ -9,17 +9,21 @@ SUNXI_G2D_SITE = https://github.com/HX2003/sunxi_g2d.git
 SUNXI_G2D_SITE_METHOD = git
 SUNXI_G2D_INSTALL_STAGING = YES
 
-define SUNXI_G2D_FIX_MAKEFILE
+
+define SUNXI_G2D_FIX_PATHS
 	sed -i 's|include /|include ./|g' $(@D)/Makefile
 	sed -i 's|/g2d_rcq|./g2d_rcq|g' $(@D)/Makefile
 	sed -i 's|/g2d_bsp|./g2d_bsp|g' $(@D)/Makefile
 endef
 
-SUNXI_G2D_POST_PATCH_HOOKS += SUNXI_G2D_FIX_MAKEFILE
+
+SUNXI_G2D_POST_PATCH_HOOKS += SUNXI_G2D_FIX_PATHS
+
 
 define SUNXI_G2D_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
 endef
+
 
 define SUNXI_G2D_INSTALL_STAGING_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/libg2d.so $(STAGING_DIR)/usr/lib/libg2d.so
