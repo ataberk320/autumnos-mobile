74ffb34fc1e4d3c93593bc82cd3c23a762fab0d3
Autixberk <atatv183@gmail.com>
2026-04-10 00:59:45 +0300

    Update g2d.mk

--- buildroot/g2d-lib/g2d.mk
+++ buildroot/g2d-lib/g2d.mk
@@ -1,24 +1,16 @@
-################################################################################
-#
-# sunxi-g2d
-#
-################################################################################
-
-SUNXI_G2D_VERSION = master
-SUNXI_G2D_SITE = $(call github,HX2003,sunxi_g2d,$(SUNXI_G2D_VERSION))
+SUNXI_G2D_VERSION = main
+SUNXI_G2D_SITE = https://github.com/HX2003/sunxi_g2d.git
+SUNXI_G2D_SITE_METHOD = git
 SUNXI_G2D_INSTALL_STAGING = YES
-
 
 define SUNXI_G2D_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
 endef
 
-
 define SUNXI_G2D_INSTALL_STAGING_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/libg2d.so $(STAGING_DIR)/usr/lib/libg2d.so
 	$(INSTALL) -D -m 0644 $(@D)/g2d_driver.h $(STAGING_DIR)/usr/include/g2d_driver.h
 endef
-
 
 define SUNXI_G2D_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/libg2d.so $(TARGET_DIR)/usr/lib/libg2d.so
