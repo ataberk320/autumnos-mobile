e589ba5e847b58851fc07e45c1a122d53c143fc3
Autixberk <atatv183@gmail.com>
2026-04-10 01:28:43 +0300

    Update g2d.mk

--- buildroot/g2d-lib/g2d.mk
+++ buildroot/g2d-lib/g2d.mk
@@ -4,18 +4,23 @@
 #
 ################################################################################
 
-SUNXI_G2D_VERSION = master
+SUNXI_G2D_VERSION = main
 SUNXI_G2D_SITE = https://github.com/cheofusi/sunxi-g2d.git
 SUNXI_G2D_SITE_METHOD = git
 SUNXI_G2D_INSTALL_STAGING = YES
 
+
+SUNXI_G2D_SUBDIR = module
+
 define SUNXI_G2D_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+	cd $(@D)/$(SUNXI_G2D_SUBDIR) && \
+	$(TARGET_CC) $(TARGET_CFLAGS) -fPIC -shared \
+		$$(ls *.c) -o ../libg2d.so -I.
 endef
 
 define SUNXI_G2D_INSTALL_STAGING_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/libg2d.so $(STAGING_DIR)/usr/lib/libg2d.so
-	$(INSTALL) -D -m 0644 $(@D)/module/g2d_driver.h $(STAGING_DIR)/usr/include/g2d_driver.h
+	$(INSTALL) -D -m 0644 $(@D)/$(SUNXI_G2D_SUBDIR)/g2d_driver.h $(STAGING_DIR)/usr/include/g2d_driver.h
 endef
 
 define SUNXI_G2D_INSTALL_TARGET_CMDS
