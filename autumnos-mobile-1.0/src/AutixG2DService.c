511788cb16ed449291c8b6e87d1bcbba172cd5c7
Autixberk <atatv183@gmail.com>
2026-04-25 17:57:34 +0300

    Update AutixG2DService.c

--- autumnos-mobile-1.0/src/AutixG2DService.c
+++ autumnos-mobile-1.0/src/AutixG2DService.c
@@ -6,27 +6,41 @@
 #include <stdint.h>
 #include <sys/ioctl.h>
 
-#include "sunxi-g2d.h"
-#define BLIT_SCREEN 1
 
-int main(int argc, char** argv) {
+#include "g2d_driver_i.h" 
+
+int main() {
+
     int g2d_fd = open("/dev/g2d", O_RDWR);
     if (g2d_fd < 0) {
         return -1;
     }
 
-    int request = 0;
-    g2d_blt config;
-    memset(&config, 0, sizeof(g2d_blt));
-    while (1) {
-        if (request == BLIT_SCREEN) {
-            if (ioctl(g2d_fd, G2D_CMD_BITBLT, &config) < 0) {
-                perror("IOCTL error!");
-            }
-            request = 0; 
-        }
+    g2d_blt blit_para;
+    memset(&blit_para, 0, sizeof(g2d_blt));
 
-        usleep(100000); 
+    // Kaynak Görüntü (Source)
+    blit_para.src_image.addr[0] = 0x45000000;
+    blit_para.src_image.w       = 800;
+    blit_para.src_image.h       = 480;
+    blit_para.src_image.format  = G2D_FORMAT_ARGB8888;
+
+
+    blit_para.dst_image.addr[0] = 0x46000000;
+    blit_para.dst_image.w       = 800;
+    blit_para.dst_image.h       = 480;
+    blit_para.dst_image.format  = G2D_FORMAT_ARGB8888;
+
+
+    blit_para.src_rect.x = 0; blit_para.src_rect.y = 0;
+    blit_para.src_rect.w = 800; blit_para.src_rect.h = 480;
+    blit_para.dst_x = 0; blit_para.dst_y = 0;
+
+    if (ioctl(g2d_fd, G2D_CMD_BITBLT, &blit_para) < 0) {
+
+    } else {
+
+        ioctl(g2d_fd, G2D_CMD_WAIT_FINISH, NULL);
     }
 
     close(g2d_fd);
