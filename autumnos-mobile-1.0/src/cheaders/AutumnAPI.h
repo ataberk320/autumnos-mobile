fd01089ec5e3fc6954c5ee00a2c52e4b99d08c16
Autixberk <atatv183@gmail.com>
2026-05-10 12:14:57 +0300

    Add files via upload

--- autumnos-mobile-1.0/src/cheaders/AutumnAPI.h
+++ autumnos-mobile-1.0/src/cheaders/AutumnAPI.h
@@ -1,9 +1,23 @@
 #ifndef API_H
 #define API_H
 
+typedef struct {
+	int x;
+	int y;
+	int pressed;
+} MouseData;
+
 void AutumnAPI_Request_PowerOff(void);
 
+void AutumnAPI_Emergency_PowerOff();
+
+int AutumnAPI_Set_MsSock();
+
+int AutumnAPI_Show_Toast(const char *msg);
+
 void AutumnAPI_Request_Reboot(void);
+
+void AutumnAPI_Read_Mouse(int sock, MouseData *data);
 
 int AutumnAPI_Read_Battery_Level(void);
 
