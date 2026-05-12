3fed09b1627ef11145f0b471478617f3dc8a26a9
Autixberk <atatv183@gmail.com>
2026-05-12 13:29:12 +0300

    Update build_gl.sh

--- autumnos-mobile-1.0/build_scripts/build_gl.sh
+++ autumnos-mobile-1.0/build_scripts/build_gl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-rm -f *.o *.so gltestapp
+rm -f *.o *.so
+
 riscv-linux-gcc -c AutumnGL_Eng.c AutumnGL_Math.c -fPIC $(pkg-config --cflags sdl2 freetype2)
+
 riscv-linux-gcc -shared -o libautumngl.so AutumnGL_Eng.o AutumnGL_Math.o $(pkg-config --libs sdl2 freetype2) -lGLESv2 -lEGL -lm
-riscv-linux-gcc gltest.c -o gltestapp -L. -lautumngl $(pkg-config --cflags --libs sdl2 freetype2) -lGLESv2 -lEGL -lm
