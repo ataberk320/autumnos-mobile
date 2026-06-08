a948f07af36dd0537159eb52f8ec39ee9d0512ab
Autixberk <atatv183@gmail.com>
2026-06-08 20:38:40 +0300

    UDS is removed: New socket Autumn ChatSock!

--- autumnos-mobile-1.0/src/API/AutumnAPI_Socket.c
+++ autumnos-mobile-1.0/src/API/AutumnAPI_Socket.c
@@ -1,62 +1,42 @@
-#include <sys/un.h>
-#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <errno.h>
-int AutumnAPI_Socket_CreateServer(const char *socket_path) {
-        int server_fd;
-        struct sockaddr_un addr;
-        unlink(socket_path);
-        server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-        if (server_fd < 0) return -1;
-        memset(&addr, 0, sizeof(addr));
-        addr.sun_family = AF_UNIX;
-        strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path) - 1);
-        if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-                close(server_fd);
-                return -1;
-        }
 
-        if (listen(server_fd, 5) < 0) {
-                close(server_fd);
-                return -1;
-        }
+#define MSG_SIZE 512
 
-        return server_fd;
+typedef struct {
+	char to_server[MSG_SIZE];
+	char to_client[MSG_SIZE];
+	int status;
+} ChatSock;
+
+ChatSock* AutumnAPI_Sock_Create(const char* name, int is_server) {
+	char path[128];
+	sprintf(path, "/%s.acsock", name);
+	int fd = shm_open(path, O_CREAT | O_RDWR, 0666);
+	if (is_server) ftruncate(fd, sizeof(ChatSock));
+	return mmap(NULL, sizeof(ChatSock), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 }
 
-int AutumnAPI_Socket_SocRead(int server_fd, int *client_fd, char *buffer, int len) {
-        
-	if (*client_fd < 0) {
-                *client_fd = accept(server_fd, NULL, NULL);
-		if (*client_fd < 0) return -1;
-        }
-        
-	int bytes = recv(*client_fd, buffer, len, 0);
-	
-	if (bytes == 0 || (bytes < 0 && errno != EAGAIN)) {
-        	close(*client_fd);
-        	*client_fd = -1;
-        	return -1;
-    	}
-
-    return bytes;
+int AutumnAPI_Listen_Friend(ChatSock *sock) {
+	return (sock->status == 1);
 }
 
-int AutumnAPI_Socket_ConnSoc(const char *socket_path) {
-        int sock = socket(AF_UNIX, SOCK_STREAM, 0);
-        struct sockaddr_un addr;
-        addr.sun_family = AF_UNIX;
-        strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
-        if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0) return -1;
-        return sock;
+void AutumnAPI_Receive_From_Friend(ChatSock *sock, char *buffer, int is_server) {
+	if (is_server) strcpy(buffer, sock->to_server);
+	else strcpy(buffer, sock->to_client);
 }
 
-void AutumnAPI_Socket_SendCmd(const char *path, const char *cmd) {
-        int fd = AutumnAPI_Socket_ConnSoc(path);
-        if (fd >= 0) {
-                send(fd, cmd, strlen(cmd), 0);
-                close(fd);
-        }
+void AutumnAPI_Send_Msg(ChatSock *sock, const char *data, int is_server) {
+    if (is_server) {
+        strcpy(sock->to_client, data);
+        sock->status = 2; 
+    } else {
+        strcpy(sock->to_server, data);
+        sock->status = 1; 
+    }
 }
