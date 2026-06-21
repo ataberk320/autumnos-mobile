9aa9c4e9092345b92d1265cb926a0aa8a9ddc8f4
Autixberk <atatv183@gmail.com>
2026-06-21 19:07:58 +0300

    Add files via upload

---
+++ aarch64/autumnos-mobile-1.0/src/API/AutumnAPI_FileSys.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include "AutumnSyscall.h"
+#include <sys/statvfs.h>
+long AutumnIO_FileSys_Get_Free_DS(const char *path) {
+        struct statvfs vfs;
+        if(statvfs(path, &vfs) != 0) return -1;
+        unsigned long free_bytes = vfs.f_bavail * vfs.f_frsize;
+        return free_bytes/(1024*1024);
+}
+void AutumnIO_Write(int fd, const void *buf, uint32_t len) {
+	AutumnSys_Syscall(SYS_WRITE, fd, (long)buf, len, 0, 0, 0);
+}
+
+int AutumnIO_Read(int fd, void *buf, uint32_t len) {
+	return (int)AutumnSys_Syscall(SYS_READ, fd, (long)buf, len, 0, 0, 0);
+}
+
+int AutumnIO_Open(const char *path, int flags) {
+	return (int)AutumnSys_Syscall(SYS_OPENAT, -100, (long)path, flags, 0, 0, 0);
+}
+
+int AutumnIO_Close(int fd) {
+	return (int)AutumnSys_Syscall(SYS_CLOSE, fd, 0, 0, 0, 0, 0);
+}
+
--- aarch64/autumnos-mobile-1.0/src/API/AutumnAPI_Socket.c
+++ aarch64/autumnos-mobile-1.0/src/API/AutumnAPI_Socket.c
@@ -1,1 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdatomic.h>
 
+#define MSG_SIZE 512
+
+typedef struct {
+	char to_server[MSG_SIZE];
+	char to_client[MSG_SIZE];
+	atomic_int status;
+	atomic_flag lock;
+} ChatSock;
+
+ChatSock* AutumnAPI_Sock_Create(const char* name, int is_server) {
+	char path[128];
+	sprintf(path, "/%s.acsock", name);
+	int fd = shm_open(path, O_CREAT | O_RDWR, 0666);
+	if (is_server) ftruncate(fd, sizeof(ChatSock));
+	ChatSock *sock = (ChatSock*)mmap(NULL, sizeof(ChatSock), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	
+	if (is_server) {
+        	atomic_flag_clear(&sock->lock);
+        	atomic_init(&sock->status, 0);
+    	}
+    
+    	return sock;
+}
+
+void AutumnAPI_LockSock(ChatSock *sock) {
+    while (atomic_flag_test_and_set(&sock->lock));
+}
+
+void AutumnAPI_UnlockSock(ChatSock *sock) {
+    atomic_flag_clear(&sock->lock);
+}
+
+int AutumnAPI_Listen_Friend(ChatSock *sock) {
+	return (sock->status == 1);
+}
+
+void AutumnAPI_Receive_From_Friend(ChatSock *sock, char *buffer, int is_server) {
+	if (is_server) strcpy(buffer, sock->to_server);
+	else strcpy(buffer, sock->to_client);
+}
+
+void AutumnAPI_Send_Msg(ChatSock *sock, const char *data, int is_server) {
+    AutumnAPI_LockSock(sock);
+	if (is_server) {
+        strncpy(sock->to_client, data, MSG_SIZE - 1);
+        atomic_store(&sock->status, 2); 
+    } else {
+        strncpy(sock->to_server, data, MSG_SIZE - 1);
+        atomic_store(&sock->status, 1); 
+    }
+	AutumnAPI_UnlockSock(sock);
+}
