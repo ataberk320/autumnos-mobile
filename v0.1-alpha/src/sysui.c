091c268f5bdc990c976913b8fc520b37f17367a0
Autixberk <atatv183@gmail.com>
2026-07-08 19:48:57 +0300

    Refactor AutumnUI_LoadResources for clarity

--- v0.1-alpha/src/sysui.c
+++ v0.1-alpha/src/sysui.c
@@ -53,25 +53,25 @@ int UI_SetFont(FT_Library *library, FT_Face *face) {
 }
 
 void AutumnUI_LoadResources() {
-        bg = img->LdImg("/usr/share/test.png");
-        if (!bg) {
-                perror("LoadImage");
-                return;
-        }
-        gfx->DrawImage(&screen, bg, 0, 0);
+    bg = img->LdImg("/usr/share/test.png");
+    if (!bg) {
+            perror("LoadImage");
+            return;
+    }
+    gfx->DrawImage(&screen, bg, 0, 0);
         
 	my_gif = img->LdGif("/usr/share/wp.gif");
-        bird = img->LdGif("/usr/share/test.gif");
-        if (!my_gif || !bird) {
-                perror("LoadGif");
-		return;
-        }
+    bird = img->LdGif("/usr/share/test.gif");
+    if (!my_gif || !bird) {
+            perror("LoadGif");
+			return;
+    }
 	
 	int ret = mp3p->LoadMp3("/home/autumnuser0/touch.mp3");
-        if (ret < 0) {
-                perror("LoadMp3");
-		return;
-        }
+    if (ret < 0) {
+            perror("LoadMp3");
+			return;
+    }
 
 }
 
@@ -85,22 +85,21 @@ void* AutumnUI(void* arg) {
 	AutumnUI_LoadResources();
 	AutumnUI_Prepare();
 	FT_Library lib;
-        FT_Face face;
+    FT_Face face;
 	int frame_count = img->CountGif(my_gif);
-        int bird_count = img->CountGif(bird);
+	int bird_count = img->CountGif(bird);
 	if (UI_SetFont(&lib, &face) == 0) {
 		printf("UI: Font successfully loaded");
-        }
-
-        else {
-                perror("UI");
-        }
+    }
+	else {
+        perror("UI");
+    }
 
 	ChatTunnel* tunn = AutumnAPI_Tunnel_Connect("mouse_pipe");
-    	if (!tunn) {
-        	perror("Tunnel");
-        	return NULL;
-    	}
+    if (!tunn) {
+        perror("Tunnel");
+        return NULL;
+    }
 
 	is_ui = true;
 
@@ -109,14 +108,14 @@ void* AutumnUI(void* arg) {
 		gfx->DrawGif(&screen, my_gif, 0, 0, current_frame); 
 		mp3p->GetFileTime(&tm.elapsed, &tm.total);
 
-        	unsigned int indicator_color = mouse_btn1 ? 0xFF00FF00 : 0xFFFFFFFF;
+        unsigned int indicator_color = mouse_btn1 ? 0xFF00FF00 : 0xFFFFFFFF;
 
-        	char coord_text[64];
+        char coord_text[64];
 		char bat[32];
 
-        	snprintf(coord_text, sizeof(coord_text), "X: %d  Y: %d %s", mouse_x, mouse_y, mouse_btn1 ? "[TOUCHED]" : "");
-        	gfx->Text(&screen, face, coord_text, 10, 20, 0xFFFFFFFF);
-        	gfx->Text(&screen, face, "No GSM device", 10, 40, 0xFFFFFFFF);
+        snprintf(coord_text, sizeof(coord_text), "X: %d  Y: %d %s", mouse_x, mouse_y, mouse_btn1 ? "[TOUCHED]" : "");
+        gfx->Text(&screen, face, coord_text, 10, 20, 0xFFFFFFFF);
+        gfx->Text(&screen, face, "No GSM device", 10, 40, 0xFFFFFFFF);
 		
 		int batper = pwr->BatPercent();
 		if (batper >= 0) {
@@ -128,26 +127,26 @@ void* AutumnUI(void* arg) {
 
 		gfx->Text(&screen, face, bat, 10, 80, 0xFFFFFFFF);
 		
-        	wid->SpawnButton(&screen, face, &btn);
+        wid->SpawnButton(&screen, face, &btn);
 		mp3p->GetFileTime(&tm.elapsed, &tm.total);
-                wid->SpawnElTimer(&screen, face, &tm);
+        wid->SpawnElTimer(&screen, face, &tm);
 		if (wid->IsTouchEvent(&btn, mouse_x, mouse_y, mouse_btn1)) {
 			pwr->Reboot();
-        	}
+        }
 	
     			
 		gfx->Text(&screen, face, "A", mouse_x, mouse_y, indicator_color);
 		gfx->RefreshScreen(&screen);
 
-        	current_frame++;
-        	if (current_frame >= frame_count) {
-        		current_frame = 0;
-        	}
+        current_frame++;
+        if (current_frame >= frame_count) {
+        	current_frame = 0;
+        }
                 
 		bird_frame++;
-        	if (bird_frame >= bird_count) {
-        		bird_frame = 0;
-        	}
+        if (bird_frame >= bird_count) {
+        	bird_frame = 0;
+        }
         
 		usleep(16666);
 	}
