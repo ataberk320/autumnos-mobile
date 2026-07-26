502f33f35e3bc7e63515a886dd1ca037d1097b6c
Autixberk <atatv183@gmail.com>
2026-07-26 15:00:19 +0300

    FIXME: Added Harfbuzz based shaping function
    EXTRA: Converted AutumnAPI_UnicodeTF8 function to shaping function wrapper for redirection if the char is 4 byte

--- v0.1-alpha/API/libatmnls.c
+++ v0.1-alpha/API/libatmnls.c
@@ -1,9 +1,20 @@
 #include <stdint.h>
+#include <hb.h>
+#include <hb-ft.h>
+#include <stdlib.h>
 
-uint32_t AutumnAPI_UnicodeTF8(const char** s) {
+void AutumnAPI_ShapeCompx(hb_font_t *hb_font, const char *txt); //description for existing symbol
+
+//FIXME: added hb_font_t parameter for redirection to shaping function.
+uint32_t AutumnAPI_UnicodeTF8(const char** s, hb_font_t *hb_font) {
 	const unsigned char* p = (const unsigned char*)*s;
 	if (!*p) return 0;
 	
+	//FIXME: added redirection to shaping function for 4 byte or special cases.
+	if (p[0] >= 0xF0) { 
+	    AutumnAPI_ShapeCompx(hb_font, *s);
+	}
+
 	if (p[0] < 0x80) {
 		*s += 1;
 		return p[0];
@@ -27,3 +38,19 @@ uint32_t AutumnAPI_UnicodeTF8(const char** s) {
 	return 0xFFFD;
 }
 
+//FIXME: added Harfbuzz based complex text shaping function for 4 byte chars.
+void AutumnAPI_ShapeCompx(hb_font_t *hb_font, const char *txt) {
+	hb_buffer_t *buf = hb_buffer_create(); //adding buffer for adding text data.
+	if (!buf) return;
+
+	hb_buffer_add_utf8(buf, utf8_text, -1, 0, -1); //adding text to buffer (*buf)
+	hb_buffer_guess_segment_properties(buf); //detecting auto language and script data on UTF8 format
+	
+	hb_shape(hb_font, buf, NULL, 0); //shaping the font
+
+	unsigned int glyph_count; //buffer for glyph data
+    	hb_glyph_info_t *glyph_infos = hb_buffer_get_glyph_infos(buf, &glyph_count); //getting glyph info
+    	hb_glyph_position_t *glyph_pos = hb_buffer_get_glyph_positions(buf, &glyph_count); //get glyph positions
+
+	hb_buffer_destroy(buf); //MEMORY ALLOC!!!
+}
