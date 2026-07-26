afcda9ed213ed0e9b1dceff6c4703f4c75fcb6f7
Autixberk <atatv183@gmail.com>
2026-07-26 15:07:59 +0300

    FIXME: Moved gradient palette API from GFX API

--- v0.1-alpha/API/libfbdev.c
+++ v0.1-alpha/API/libfbdev.c
@@ -162,16 +162,7 @@ void AutumnAPI_DrawString(FbDev* fb, FT_Face face, const char* text, int x, int 
     }
 }
 
-uint32_t GetGradientColor(uint32_t base_color, int y, int start_y, int height) {
-    int factor = ((y - start_y) * 40 / height) - 20;
-    int r = (base_color >> 16) & 0xFF;
-    int g = (base_color >> 8) & 0xFF;
-    int b = base_color & 0xFF;
-    r = (r + factor > 255) ? 255 : (r + factor < 0) ? 0 : r + factor;
-    g = (g + factor > 255) ? 255 : (g + factor < 0) ? 0 : g + factor;
-    b = (b + factor > 255) ? 255 : (b + factor < 0) ? 0 : b + factor;
-    return (0xFF << 24) | (r << 16) | (g << 8) | b;
-}
+//UPDATE: palette functions are removed from FBDEV API!
 
 void AutumnAPI_DrawButton(FbDev* fb, FT_Face face, int x, int y, int w, int h, int r, uint32_t bg_color, const char* text, uint32_t text_color) {
     uint32_t border_color = 0xFF808080;
