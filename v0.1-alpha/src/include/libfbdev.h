3212dc446cff8255da4487bbf752b655bcc26a7f
Autixberk <atatv183@gmail.com>
2026-08-04 19:44:21 +0300

    Remove unused function declarations from libfbdev.h
    Removed several function declarations related to drawing and framebuffer management.

--- v0.1-alpha/src/include/libfbdev.h
+++ v0.1-alpha/src/include/libfbdev.h
@@ -19,17 +19,4 @@ typedef struct {
 	DRMStruct* hal_ctx;
 } FbDev;
 
-uint32_t AutumnAPI_UnicodeTF8(const char** s);
-void AutumnAPI_DrawPix(FbDev* fb, int x, int y, uint32_t color);
-void AutumnAPI_DrawRect(FbDev* fb, int x, int y, int w, int h, uint32_t color, int fill);
-void AutumnAPI_DrawButton(FbDev* fb, FT_Face face, int x, int y, int w, int h, int r, uint32_t bg_color, const char* text, uint32_t text_color, AutumnImage* icon);
-void AutumnAPI_DrawImg(FbDev* fb, AutumnImage* img, int x, int y);
-void AutumnAPI_DrawString(FbDev* fb, FT_Face face, const char* text, int x, int y, uint32_t color);
-
-int AutumnAPI_Init(FbDev* fb, const char *path);
-void AutumnAPI_URandom(FbDev* fb);
-void AutumnAPI_ClearFb(FbDev* fb, uint32_t color);
-int AutumnAPI_FbRefresh(FbDev* fb);
-void AutumnAPI_FbReset(DRMStruct* fb);
-
 #endif
