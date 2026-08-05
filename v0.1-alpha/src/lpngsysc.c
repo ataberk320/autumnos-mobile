5f15f310a949a7e17cd9d2702bcaf907392693a9
Autixberk <atatv183@gmail.com>
2026-08-05 13:03:20 +0300

    UPDATE: Implement libpng loading callback with AutumnIO
    Added a callback function for libpng to use AutumnIO for loading.

--- v0.1-alpha/src/lpngsysc.c
+++ v0.1-alpha/src/lpngsysc.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <sys/types.h>
 #include "io.h"
-
+//UPDATE: added AutumnIO implementated libpng loading callback
 void _png_RdCallback(png_structp png_ptr, png_bytep outBytes, png_size_t byteCountToRead) {
 	int fd = (int)(intptr_t)png_get_io_ptr(png_ptr);
 	ssize_t bytesRead = _AutumnSys_ioRead(fd, outBytes, byteCountToRead
