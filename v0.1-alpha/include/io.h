310128e74b79c07087d83a490b3519037ecca938
Autixberk <atatv183@gmail.com>
2026-08-07 15:04:24 +0300

    Fixed conflicting definition error.

--- v0.1-alpha/include/io.h
+++ v0.1-alpha/include/io.h
@@ -11,6 +11,7 @@
 #define SYS_LINUX_CLOSE  57
 #define SYS_LINUX_RENAME 38
 #define SYS_LINUX_UNLINK 102
+#define SYS_LINUX_LSEEK  62
 
 static inline long _AutumnSys_Syscall3(long n, long a0, long a1, long a2) {
         register long ret __asm__("a0") = a0;
@@ -55,28 +56,32 @@ static inline long _AutumnSys_Syscall1(long n, long a0) {
     return ret;
 }
 
-int _AutumnSys_ioOpen(const char* path, int flags, int mode) {
+static inline int _AutumnSys_ioOpen(const char* path, int flags, int mode) {
         return (int)_AutumnSys_Syscall3(SYS_LINUX_OPEN, (long)path, (long)flags, (long)mode);
 }
 
-ssize_t _AutumnSys_ioRead(int fd, void* buf, size_t count) {
+static inline ssize_t _AutumnSys_ioRead(int fd, void* buf, size_t count) {
         return _AutumnSys_Syscall3(SYS_LINUX_READ, (long)fd, (long)buf, (long)count);
 }
 
-ssize_t _AutumnSys_ioWrite(int fd, const void* buf, size_t count) {
+static inline ssize_t _AutumnSys_ioWrite(int fd, const void* buf, size_t count) {
         return _AutumnSys_Syscall3(SYS_LINUX_WRITE, (long)fd, (long)buf, (long)count);
 }
 
-int _AutumnSys_ioClose(int fd) {
+static inline int _AutumnSys_ioClose(int fd) {
         return (int)_AutumnSys_Syscall2(SYS_LINUX_CLOSE, (long)fd, 0);
 }
 
-int _AutumnSys_ioRename(const char* old_p, const char* new_p) {
+static inline int _AutumnSys_ioRename(const char* old_p, const char* new_p) {
     return (int)_AutumnSys_Syscall2(SYS_LINUX_RENAME, (long)old_p, (long)new_p);
 }
 
-int _AutumnSys_ioUnlink(const char* path) {
+static inline int _AutumnSys_ioUnlink(const char* path) {
     return (int)_AutumnSys_Syscall1(SYS_LINUX_UNLINK, (long)path);
+}
+
+static inline off_t _AutumnSys_ioLseek(int fd, off_t offset, int whence) {
+	return (off_t)_AutumnSys_Syscall3(SYS_LINUX_LSEEK, (long)fd, (long)offset, (long)whence);
 }
 
 #endif
