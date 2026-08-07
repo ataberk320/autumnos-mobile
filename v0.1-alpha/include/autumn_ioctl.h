11d6dd55fbbfca6d1986f336ccbf10ffe2cd6f41
Autixberk <atatv183@gmail.com>
2026-08-07 15:12:41 +0300

    Update autumn_ioctl.h

--- v0.1-alpha/include/autumn_ioctl.h
+++ v0.1-alpha/include/autumn_ioctl.h
@@ -6,13 +6,13 @@
 
 #define G2D_IOC_MAGIC 'g'
 #define SIM_IOC_MAGIC 's'
-#define IOC_M 'a'
+#define TABL_IOC_MAGIC 'a'
 #define IOCG2DBITBLT _IOW(G2D_IOC_MAGIC, 0x40, unsigned long)
 #define IOCMODSCAN       _IO(SIM_IOC_MAGIC, 1)
 #define IOCMODGETSTAT     _IOR(SIM_IOC_MAGIC, 2, int)
 #define IOCMODSNDCMD   _IOW(SIM_IOC_MAGIC, 3, char*)
 #define IOCMODRDRESP  _IOR(SIM_IOC_MAGIC, 4, char*)
-#define IOCTBLREGSRV _IOW(IOC_M, 1, unsigned long)
-#define IOCTBLGETSRV _IOR(IOC_M, 2, unsigned long)
+#define IOCTBLREGSRV _IOW(TABL_IOC_MAGIC, 1, unsigned long)
+#define IOCTBLGETSRV _IOR(TABL_IOC_MAGIC, 2, unsigned long)
 
 #endif
