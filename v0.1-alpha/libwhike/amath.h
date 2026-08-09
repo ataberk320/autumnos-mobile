f2e29c413b2ad056316318d271ea85d0115327be
Autixberk <atatv183@gmail.com>
2026-08-09 12:50:14 +0300

    Added headers for Whike C library

--- v0.1-alpha/libwhike/amath.h
+++ v0.1-alpha/libwhike/amath.h
@@ -1,1 +1,12 @@
+#ifndef M_H
+#define M_H
 
+double afabs(double x);
+double asqrt(double x);
+double apow(double base, int exp);
+double asin(double x);
+double acos(double x);
+double atan(double x);
+double alog(double x);
+
+#endif
---
+++ v0.1-alpha/libwhike/atime.h
@@ -0,0 +1,8 @@
+#ifndef TIME_H
+#define TIME_H
+
+
+uint64_t atick_ns(void);
+uint64_t atick_us(void);
+uint64_t atick_ms(void);
+uint64_t atick_sec(void);
